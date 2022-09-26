af1fc20fbd7fa0eba762104d55136722142ed58a
naveen1555 <kasukurthinaveen11@gmail.com>
2022-09-26 05:09:34 +0000

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
+import LoginForm from './components/LoginForm'
+import ProtectedRoute from './components/ProtectedRoute'
+import NotFound from './components/NotFound'
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/ebank/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
